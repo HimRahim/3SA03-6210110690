ab4268144b41c629600050ed9432612c4ba49818
HimRahim <6210110690@psu.ac.th>
2021-07-05 12:52:03 +0700

    commit character card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './App.css';
 import CharacterCard from './CharacterCard';
 
 const word = "Hello";
 function App() {
   return (
-    <div className="App">
+    <div>
         {
           Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
         }
