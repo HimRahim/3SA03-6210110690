e735f3ae80492910f50d6c7b0b9447cf5691fbe6
HimRahim <6210110690@psu.ac.th>
2021-07-05 12:37:50 +0700

    commit character card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import './App.css';
+import CharacterCard from './CharacterCard';
 
 function App() {
   return (
     <div className="App">
-     {"Hello World"}
+        <CharacterCard value='h'/>
+        <CharacterCard value='i'/>
     </div>
+    
   );
 }
 
